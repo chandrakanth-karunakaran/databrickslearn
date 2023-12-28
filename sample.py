0543e7829adabc734fea99b411af182aa0f85078
chandrakanth-karunakaran <chandrakanth-karunakaran>
2023-12-28 09:41:21 +0000

    tes commit

---
+++ sample.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Learning Databricks
+# MAGIC
+# MAGIC ![databricks](https://justglobal.com/wp-content/uploads/2022/07/databricks-logo.png)
+
+# COMMAND ----------
+
+# MAGIC %run sampleold
+
+# COMMAND ----------
+
+name
+
+# COMMAND ----------
+
+display(dbutils.fs.ls('/databricks-datasets/'))
+
+# COMMAND ----------
+
+
